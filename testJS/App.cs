00818e6110c7fe8c6114834b7be0314b9d1ecffb
zedr0n <zedr0nre@gmail.com>
2015-08-16 23:58:40 +0100

    initial commit

---
+++ testJS/App.cs
@@ -0,0 +1,15 @@
+﻿using Bridge;
+using Bridge.Html5;
+
+namespace testJS
+{
+    public class App
+    {
+        [Ready]
+        public static void Main()
+        {
+            // Simple alert() to confirm it's working
+            Global.Alert("Success");
+        }
+    }
+}
