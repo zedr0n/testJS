2d4425578b2a62643d3918d51154ff1ff87e2d43
Zedr0n <zedr0nre@gmail.com>
2015-08-29 11:16:01 +0100

    change class organisation

---
+++ testJS/Area.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bridge.jQuery2;
+
+namespace testJS
+{
+    public class Area
+    {
+        //public int initialHeight;
+        //public int initialOuterHeight;
+        protected jQuery jquery;
+        protected Margin _margin;
+        protected OuterHeight _outerHeight;
+        protected Height _height;
+
+        protected List<Property<int>> properties;
+
+        protected bool cssUpdated = false;
+
+        // disable default constructor
+        private Area() { }
+        public Area(string selection)
+        {
+            jquery = jQuery.Select(selection);
+            //initialHeight = jquery.Height();
+            //initialOuterHeight = jquery.OuterHeight(true);
+
+            // create properties
+            _margin = new Margin(jquery,this);
+            _outerHeight = new OuterHeight(jquery,this);
+            _height = new Height(jquery,this);
+
+            // update properties list
+            properties.Add(_margin);
+            properties.Add(_outerHeight);
+            properties.Add(_height);
+        }
+
+        public int margin
+        {
+            get
+            {
+                return _margin.get();
+            }
+            set
+            {
+                _margin.set(value);
+            }
+        }
+        public int outerHeight
+        {
+            get
+            {
+                return _outerHeight.get();
+            }
+            set { _outerHeight.set(value); }
+        }
+        public virtual int height
+        {
+            get
+            {
+                return _height.get();
+            }
+            set
+            {
+                cssUpdated = _height.set(value);
+
+                // notify properties that CSS changed
+                foreach (Property<int> prop in properties)
+                    prop.syncFromCSS = cssUpdated;
+            }
+        }
+
+        public int initialHeight
+        {
+            get
+            {
+                return _height.init;
+            }
+        }
+
+        public int initialOuterHeight
+        {
+            get
+            {
+                return _outerHeight.init;
+            }
+        }
+    }
+    public class Header : Area
+    {
+        private Area images;
+
+        public Header() :
+            base(".header")
+        {
+            images = new Area("img");
+        }
+
+        public override int height
+        {
+            get
+            {
+                return base.height;
+            }
+            set
+            {
+                base.height = value;
+                images.height = images.initialHeight + (base.height - base.initialHeight);
+            }
+        }
+    }
+    public class Content : Area
+    {
+        public Content() :
+            base(".content") { }
+    }
+    public class Footer : Area
+    {
+        public Footer() :
+            base(".footer") { }
+    }
+}
---
+++ testJS/Property.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bridge.jQuery2;
+
+namespace testJS
+{
+    public delegate void cssUpdateEventHandler(object sender,EventArgs e);
+    public delegate void changeEventHandler(object sender, EventArgs e);
+
+    public class Property<T>
+    {
+        protected jQuery jquery;
+        protected Area area;
+
+        public event cssUpdateEventHandler cssUpdate;
+        public event changeEventHandler change;
+
+        public bool syncFromCSS = false;
+        public bool toUpdateCSS = true;
+
+        protected T value;
+        public T init;
+
+        protected Property() { }
+        public Property(jQuery jquery, Area area)
+        {
+            this.jquery = jquery;
+            this.area = area;
+            read();
+            init = value;
+        }
+
+        public bool set(T value)
+        {
+            this.value = value;
+            if (toUpdateCSS)
+            {
+                write();
+                toUpdateCSS = false;
+                syncFromCSS = true;
+            }
+
+            return syncFromCSS;
+        }
+
+        public T get()
+        {
+            return value;
+        }
+
+        protected virtual void onCssUpdate(EventArgs e)
+        {
+            if (cssUpdate != null)
+                cssUpdate(this, e);
+        }
+
+        protected virtual void onChange(EventArgs e)
+        {
+            if (change != null)
+                change(this, e);
+        }
+
+        protected virtual void read() { }
+        protected virtual void write()
+        {
+        }
+
+    }
+    public class Margin : Property<int>
+    {
+        protected Margin() { }
+        public Margin(jQuery jquery, Area area) : base(jquery,area) { }
+
+        protected override void read()
+        {
+            value = jquery.OuterHeight(true) - jquery.OuterHeight(false);
+        }
+    }
+    public class OuterHeight : Property<int>
+    {
+        protected OuterHeight() { }
+        public OuterHeight(jQuery jquery,Area area) : base(jquery,area) { }
+
+        protected override void read()
+        {
+            value = jquery.OuterHeight(true);
+        }
+    }
+    public class Height : Property<int>
+    {
+        protected Height() { }
+        public Height(jQuery jquery, Area area) : base(jquery,area) { }
+
+        protected override void read()
+        {
+            value = jquery.Height();
+        }
+
+        protected override void write()
+        {
+            jquery.Height(value);
+        }
+
+    }
+
+}
