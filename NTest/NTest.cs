c2459f6dd1269f37d3c9be2c7952f64c4b2c4767
Zedr0n <zedr0nre@gmail.com>
2015-09-12 13:28:23 +0100

    move tests to separate project

--- NTest/NTest.cs
+++ NTest/NTest.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 using NUnit.Framework;
+using xmlGen;
 using JSHandlers;
 
-namespace xmlGen
+namespace NTest
 {
     [TestFixture]
-    class NTest
+    public class NTest
     {
         private static string xmlResult = "<MethodParser><methods><Method><name>onClick</name><parameters><Param><paramType>string</paramType><paramName>textInput</paramName></Param></parameters><returnType>string</returnType></Method><Method><name>onTest</name><parameters /><returnType>void</returnType></Method><Method><name>onTest2</name><parameters><Param><paramType>double</paramType><paramName>x</paramName></Param><Param><paramType>double</paramType><paramName>y</paramName></Param></parameters><returnType>double</returnType></Method></methods></MethodParser>";
 
@@ -18,6 +19,12 @@ namespace xmlGen
         public void testButtonXML()
         {
             Assert.AreEqual(xmlResult, (new MethodParser(new ButtonHandler())).writeToXML());
+        }
+
+        [Test]
+        public void testXML()
+        {
+            Assert.AreEqual(xmlResult, (new MethodParser()).writeToXML());
         }
     }
 }
